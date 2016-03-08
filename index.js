ad6dcbcc315e03c4815d12a9745c59c87d8b3730
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-08 17:18:14 -0500

    refactor

--- index.js
+++ index.js
@@ -7,8 +7,27 @@
 
 'use strict';
 
-var compact = require('arrayify-compact');
+var isWhitespace = require('is-whitespace');
 
-module.exports = function condense(str, sep) {
-  return compact(str.split(/[\r?\n]+/g)).join(sep || '\n');
+module.exports = function(str, options) {
+  options = options || {};
+  var sep = options.sep || '\n\n';
+  var min = options.min;
+  var re;
+
+  if (typeof min === 'number' && min !== 2) {
+    re = new RegExp('(\\r\\n|\\n){' + min + ',}');
+  }
+  re = options.regex || /(\r\n|\n){2,}/g;
+
+  // if a line is 100% whitespace, remove the whitespace (but not the line, yet)
+  if (options.keepWhitespace !== true) {
+    str = str.split('\n').map(function(line) {
+      if (/^\s+$/.test(line)) {
+        return line.trim();
+      }
+      return line;
+    }).join('\n');
+  }
+  return str.replace(re, sep);
 };
